503199425dcfeed16535dd597889e024e4ebacfe
harleydi <disneyharley26@gmail.com>
2023-07-17 00:15:12 -0400

    added delete feature

--- src/Components/ToDoCard.js
+++ src/Components/ToDoCard.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './ToDoCard.css'
+import { useOutletContext, useParams } from 'react-router-dom'
 
-const ToDoCard = ({ todo }) => {
+const ToDoCard = ({ todo, url }) => {
+  const { setRefresh } = useOutletContext()
+  const { id } = useParams()
+  // console.log(id)
+  
+
+  const handleCompleted = async () => {
+    // setRefresh(true)
+    console.log(todo._id)
+    const body = {
+      isComplete: !todo.isComplete
+    }
+    const response = await fetch(`${url}/todos/update-completed/${todo._id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "appliction/json"
+      },
+      body: JSON.stringify(body)
+    })
+    const data = await response.json()
+    console.log(data.data.isComplete)
+    // setRefresh(false)
+  }
+
+  const handleDelete = async () => {
+    setRefresh(true)
+    const response = await fetch(`${url}/todos/delete-one/${todo._id}`, { method: 'DELETE' })
+    const data = await response.json()
+    setRefresh(false)
+  }
+
   return (
-    <div>
+    <div className='card'>
         <h2>{todo.title}</h2>
         <p>{todo._id}</p>
         <p>{todo.content}</p>
@@ -11,6 +43,8 @@ const ToDoCard = ({ todo }) => {
         <p>{todo.creationDate}</p>
         <p>{todo.lastModified}</p>
         <p>{todo.completedDate !== null && todo.completedDate}</p>
+        <button onClick={handleCompleted}>Toggle Complete</button>
+        <button onClick={handleDelete}>Delete</button>
     </div>
   )
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,15 +3,15 @@ import { useOutletContext } from 'react-router-dom'
 import ToDoCard from '../Components/ToDoCard'
 
 const HomePage = () => {
-  const { todoList } = useOutletContext()
+  const { todoList, url } = useOutletContext()
 
   const todos = todoList.data
     console.log(todos)
   return (
     <div>
         <h1>Fullstack ToDo Application</h1>
-        <div>
-            {todos && todos.map((todo) => <ToDoCard key={todo._id} todo={todo} />)}
+        <div style={{ display: 'flex', gap: '2rem'}}>
+            {todos && todos.map((todo) => <ToDoCard key={todo._id} todo={todo} url={url} />)}
         </div>
     </div>
   )
