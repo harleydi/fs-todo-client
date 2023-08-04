73b37e89e6dd1432f8324aa6a4940685dca9376c
harleydi <disneyharley26@gmail.com>
2023-08-04 00:20:40 -0400

    fixed update and toggle complete

--- src/Components/ToDoCard.js
+++ src/Components/ToDoCard.js
@@ -4,26 +4,56 @@ import { useOutletContext, useParams } from 'react-router-dom'
 
 const ToDoCard = ({ todo, url }) => {
   const { setRefresh } = useOutletContext()
-  const { id } = useParams()
-  // console.log(id)
   
 
-  const handleCompleted = async () => {
-    // setRefresh(true)
-    console.log(todo._id)
-    const body = {
-      isComplete: !todo.isComplete
+  const [completeStatus, setCompleteStatus] = useState(todo.isComplete)
+  const [editTitle, setEditTitle] = useState(todo.title)
+  const [editContent, setEditContent] = useState(todo.description)
+  const [editPriority, setEditPriority] = useState("")
+  const [toggleEdit, setToggleEdit] = useState(false)
+  console.log(completeStatus)
+
+  const handleEdit = async () => {
+    setToggleEdit(true)
+  }
+
+  const handleSubmitEdit = async () => {
+    setRefresh(true)
+    const editData = {
+      title: editTitle,
+      description: editContent,
+      priority: editPriority
     }
-    const response = await fetch(`${url}/todos/update-completed/${todo._id}`, {
+    const response = await fetch(`${url}/todos/update-one/${todo._id}`, {
       method: 'PUT',
       headers: {
-        "Content-Type": "appliction/json"
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(editData)
+    })
+    const data = await response.json()
+    setToggleEdit(false)
+    setRefresh(false)
+  }
+  
+  const handleCompleted = async () => {
+    setRefresh(true)
+    console.log(todo._id)
+    setCompleteStatus(!completeStatus)
+    console.log(completeStatus)
+    const body = {
+      isComplete: completeStatus
+    }
+    const response = await fetch(`${url}/todos/update-one/${todo._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(body)
     })
     const data = await response.json()
-    console.log(data.data.isComplete)
-    // setRefresh(false)
+    
+    setRefresh(false)
   }
 
   const handleDelete = async () => {
@@ -34,18 +64,48 @@ const ToDoCard = ({ todo, url }) => {
   }
 
   return (
-    <div className='card'>
-        <h2>{todo.title}</h2>
-        <p>{todo._id}</p>
-        <p>{todo.content}</p>
-        <p>{todo.priority}</p>
-        <p>{todo.isComplete ? 'Complete' : 'Incomplete'}</p>
-        <p>{todo.creationDate}</p>
-        <p>{todo.lastModified}</p>
-        <p>{todo.completedDate !== null && todo.completedDate}</p>
-        <button onClick={handleCompleted}>Toggle Complete</button>
-        <button onClick={handleDelete}>Delete</button>
-    </div>
+    <>
+      {toggleEdit ? (
+        <div className='card'>
+          <h2>
+            Title:
+            <input value={editTitle} onChange={(e) => setEditTitle(e.target.value)} />
+          </h2>
+          <p>{todo._id}</p>
+          <label>
+            Content:
+            <input value={editContent} onChange={(e) => setEditContent(e.target.value)} />
+          </label>
+          <br />
+          <select onChange={(e) => setEditPriority(e.target.value)}>
+            <option value={'Low'}>Low</option>
+            <option value={'Medium'}>Medium</option>
+            <option value={'High'}>High</option>
+          </select>
+          <p>{todo.isComplete ? 'Complete' : 'Incomplete'}</p>
+          <p>{todo.creationDate}</p>
+          <p>{todo.lastModified}</p>
+          <p>{todo.completedDate !== null && todo.completedDate}</p>
+          <button onClick={handleSubmitEdit}>Submit Edit</button>
+          <button onClick={handleCompleted}>Toggle Complete</button>
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      ) : (
+        <div className='card'>
+            <h2>{todo.title}</h2>
+            <p>{todo._id}</p>
+            <p>{todo.description}</p>
+            <p>{todo.priority}</p>
+            <p>{todo.isComplete ? 'Complete' : 'Incomplete'}</p>
+            <p>{todo.creationDate}</p>
+            <p>{todo.lastModified}</p>
+            <p>{todo.completedDate !== null && todo.completedDate}</p>
+            <button onClick={handleEdit}>Toggle Edit</button>
+            <button onClick={handleCompleted}>Toggle Complete</button>
+            <button onClick={handleDelete}>Delete</button>
+        </div>
+      )}
+    </>
   )
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useOutletContext } from 'react-router-dom'
 import ToDoCard from '../Components/ToDoCard'
 
 const HomePage = () => {
   const { todoList, url } = useOutletContext()
 
+
+
   const todos = todoList.data
     console.log(todos)
   return (
     <div>
+      
         <h1>Fullstack ToDo Application</h1>
         <div style={{ display: 'flex', gap: '2rem'}}>
             {todos && todos.map((todo) => <ToDoCard key={todo._id} todo={todo} url={url} />)}
